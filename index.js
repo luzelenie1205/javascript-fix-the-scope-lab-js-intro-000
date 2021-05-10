137c09108c1d37ae41d2223b3c60a2f9e59ae52e
bright-generator-f905 <luzelenie1205@gmail.com>
2021-05-10 03:28:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,10 +28,10 @@ function add2(n) {
 
 var funkyFunction = function() {
   return function() {
-    return "theFUNKY!"
+    return "FUNKY!"
   }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+ theFunk = funkyFunction
