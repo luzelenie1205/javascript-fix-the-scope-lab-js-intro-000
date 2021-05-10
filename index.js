499eb3dacc0f0c29a3a6ad88a2f2d62f008b12ce
bright-generator-f905 <luzelenie1205@gmail.com>
2021-05-10 02:30:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ return function yourAnimal()
 var a = 'cat';
 return animal;
 }
-var b = yourAnimal();
-b;
+
 // The tests expect this function to return `animal` just like the previous function
   // However, you cannot simply modify the existing variable declared on line 1 in the global scope
   // How can we make sure that this function
