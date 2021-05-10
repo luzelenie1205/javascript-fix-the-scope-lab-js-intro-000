16985e422036d269e4c8d873c36756d74a589fc9
bright-generator-f905 <luzelenie1205@gmail.com>
2021-05-10 03:08:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ function myAnimal() {
   // You should not need to modify this function
   return animal}
 
+animal='cat'
 function yourAnimal()
 {
-var yourAnimal= yourAnimal();
-yourAnimal = 'cat'
-
 return animal
 }
 
