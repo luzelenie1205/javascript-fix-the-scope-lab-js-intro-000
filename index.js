f588117653b5f05e461567d063695e7d1dfd8721
bright-generator-f905 <luzelenie1205@gmail.com>
2021-05-10 02:16:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,15 +4,20 @@ function myAnimal() {
   // You should not need to modify this function
   return animal
 }
+yourAnimal();
 
-function yourAnimal() {
-  // The tests expect this function to return `animal` just like the previous function
-  // However, you cannot simply modify the existing variable declared on line 1 in the global scope 
+function yourAnimal()
+{
+console.log("cat");
+return animal
+}
+
+// The tests expect this function to return `animal` just like the previous function
+  // However, you cannot simply modify the existing variable declared on line 1 in the global scope
   // How can we make sure that this function
   // and the above function both pass?
-  // P.S.: Hard-coding 'cat' below will not work
-  return animal
-}
+  // P.S.: Hard-coding 'cat' below will not wor
+
 
 function add2(n) {
   return n + two
